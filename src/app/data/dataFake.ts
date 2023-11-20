453696dd92566b25cf72df21385a33e43dcee391
Fabio Aragao Tessaro <fabiot92@gmail.com>
2023-11-20 13:06:39 -0300

    desafio
    desafio

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,28 @@
+// Adicione esta função no seu arquivo
+function generateRandomDescription() {
+  const adjectives = ['Emocionante', 'Incrível', 'Fantástico', 'Surpreendente', 'Aventuresco', 'Divertido'];
+  const nouns = ['filme', 'série', 'evento', 'anúncio', 'lançamento', 'projeto'];
+  const verbs = ['anuncia', 'traz', 'destaca', 'mostra', 'apresenta', 'revela'];
+
+  const getRandomElement = (array: string[]) => array[Math.floor(Math.random() * array.length)];
+
+  const description = `${getRandomElement(adjectives)} ${getRandomElement(verbs)} um novo ${getRandomElement(nouns)}. Confira agora!`;
+
+  return description;
+}
+
+// Modifique a estrutura dos seus dados com a chamada da função
 export const dataFake = [
   {
-    "id":"1",
+    "id": "1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "description": generateRandomDescription(),
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
-    "id":"2",
+    "id": "2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "description": generateRandomDescription(),
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   }
-]
+];
